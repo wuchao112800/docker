16fffbf891763498ad08ba4cb2e94c1fe4120adb
wuchao <chao.wu@ynashk.com>
2019-09-03 16:26:49 +0800

    Merge branch 'master' of https://github.com/wuchao112800/docker
    # Conflicts:
    #	.gitignore

--- src/main/java/com/example/docker/Controller/DockerController.java
+++ src/main/java/com/example/docker/Controller/DockerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerController {
     @RequestMapping("/hello")
     public String hello() {
+        //git dev
         return "<h1>Hello Docker 2019/09/02</h1>";
     }
 }
